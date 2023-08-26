95597e4952af38db0ba4d92fb61e83aa433b2020
Tanu0798 <142480613+Tanu0798@users.noreply.github.com>
2023-08-26 16:30:42 +0530

    Add files via upload

---
+++ puthon file.py
@@ -0,0 +1,47 @@
+import random
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "scissors" and computer_choice == "paper") or \
+         (user_choice == "paper" and computer_choice == "rock"):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def main():
+    user_score = 0
+    computer_score = 0
+
+    print("Welcome to Rock-Paper-Scissors Game!")
+
+    while True:
+        print("\nChoose: rock, paper, or scissors")
+        user_choice = input("Your choice: ").lower()
+
+        if user_choice not in ["rock", "paper", "scissors"]:
+            print("Invalid choice. Please choose rock, paper, or scissors.")
+            continue
+        computer_choice = random.choice(["rock", "paper", "scissors"])
+
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+
+        if result == "You win!":
+            user_score += 1
+        elif result == "Computer wins!":
+            computer_score += 1
+
+        print(f"Your score: {user_score} | Computer score: {computer_score}")
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing!")
+            break
+
+if __name__ == "__main__":
+    main()
